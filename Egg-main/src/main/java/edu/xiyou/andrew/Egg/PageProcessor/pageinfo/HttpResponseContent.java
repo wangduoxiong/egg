354bf6f9e1b42d54b31f6c62c16c8dcdeac82635
wangduoxiong <630841816@qq.com>
2015-01-18 13:47:52 +0800

    增加HttpResponseContent类

---
+++ Egg-main/src/main/java/edu/xiyou/andrew/Egg/PageProcessor/pageinfo/HttpResponseContent.java
@@ -0,0 +1,36 @@
+package edu.xiyou.andrew.Egg.PageProcessor.pageinfo;
+
+import java.nio.charset.Charset;
+
+/**
+ * Created by andrew on 15-1-18.
+ */
+public class HttpResponseContent {
+    private String charset;
+    private byte[] content;
+    private String url;
+
+    public String getCharset() {
+        return charset;
+    }
+
+    public void setCharset(String charset) {
+        this.charset = charset;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
