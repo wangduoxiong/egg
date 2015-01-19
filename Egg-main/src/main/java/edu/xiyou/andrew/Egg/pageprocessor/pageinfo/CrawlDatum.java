d4f72d4fb9bb2671a1f76df2dc1e7d211884ec7e
wangduoxiong <630841816@qq.com>
2015-01-19 16:48:33 +0800

    增加CrawlDatum类

---
+++ Egg-main/src/main/java/edu/xiyou/andrew/Egg/pageprocessor/pageinfo/CrawlDatum.java
@@ -0,0 +1,51 @@
+package edu.xiyou.andrew.Egg.pageprocessor.pageinfo;
+
+/**
+ * 记录爬取种子的状态
+ * Created by andrew on 15-1-19.
+ */
+public class CrawlDatum {
+    private String url;
+    private long fetchTime; //爬取时间
+    private byte status;    //该种子的状态
+
+    /**
+     * 未爬取状态
+     */
+    public final byte CRAWLDATUM_UNFETCH = 1;
+    /**
+     * 已爬取状态
+     */
+    public final byte CRAWLDATUM_FETCHED = 2;
+    /**
+     * 已注入
+     */
+    public final byte CRAWLDATUM_INJECTED = 4;
+
+    public CrawlDatum(String url, byte status) {
+        this.url = url;
+        this.status = status;
+    }
+
+    public CrawlDatum(String url, long fetchTime, byte status) {
+        this.url = url;
+        this.fetchTime = fetchTime;
+        this.status = status;
+    }
+
+    public long getFetchTime() {
+        return fetchTime;
+    }
+
+    public void setFetchTime(long fetchTime) {
+        this.fetchTime = fetchTime;
+    }
+
+    public byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(byte status) {
+        this.status = status;
+    }
+}
