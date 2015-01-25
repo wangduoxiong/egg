574f029771fa1b87889abbdf20c61480962bac9b
wangduoxiong <630841816@qq.com>
2015-01-25 17:35:05 +0800

    Handle

---
+++ Egg-main/src/main/java/edu/xiyou/andrew/Egg/fetcher/Handle.java
@@ -0,0 +1,31 @@
+package edu.xiyou.andrew.Egg.fetcher;
+
+import edu.xiyou.andrew.Egg.net.HttpRequest;
+import edu.xiyou.andrew.Egg.pageprocessor.pageinfo.HttpResponse;
+import edu.xiyou.andrew.Egg.pageprocessor.pageinfo.Link;
+
+import java.util.List;
+
+/**
+ * Created by andrew on 15-1-25.
+ */
+public interface Handle {
+
+    /**
+     * 下载成功的处理方式
+     * @param response
+     */
+    public void onSuccess(HttpResponse response);
+
+    /**
+     * 下载失败的处理方式
+     * @param response
+     */
+    public void onError(HttpResponse response);
+
+    /**
+     * @param response
+     * @return
+     */
+    public List<String> visitAndGetNextLinks(HttpResponse response);
+}
